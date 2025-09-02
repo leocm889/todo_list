e6588258a3c744f99fe2667ebebf7f2334a3c0d5
Bassam889 <leocm889+GitHub@gmail.com>
2025-09-02 07:29:01 -0400

    imported dependencies to turn this project into a real CLI program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, Subcommand};
 mod menu;
 mod priority;
 mod status;
@@ -7,4 +8,25 @@ use menu::display_menu;
 fn main() {
     let file_path = "todos.json";
     display_menu(file_path);
+}
+
+#[derive(Parser)]
+#[command(name = "todo")]
+#[command(about = "A simple todo shell program", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        title: String,
+        description: String,
+        #[arg(long, default_value = "medium")]
+        priority: String,
+        #[arg(long, default_value = "pending")]
+        status: String,
+    },
+    List,
 }
